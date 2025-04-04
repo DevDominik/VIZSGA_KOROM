6c8646537ceb787b5d12002729b3150e4598813c
kecskes.dominik <kecskes.dominik@BURG-1E06-02>
2025-04-04 09:15:18 +0200

    asd

---
+++ AutokConsole/Models/Auto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutokConsole.Models
+{
+    class Auto
+    {
+        static List<Auto> cars = new List<Auto>();
+        public string Serial { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Sold { get; set; }
+        public int AvgPrice { get; set; }
+        public string Brand { get; set; }
+        public Auto(string line) 
+        { 
+            string[] parts = line.Split(';');
+            Serial = parts[0];
+            Brand = parts[1];
+            Model = parts[2];
+            Year = int.Parse(parts[3]);
+            Color = parts[4];
+            Sold = int.Parse(parts[5]);
+            AvgPrice = int.Parse(parts[6]);
+        }
+        public static void LoadFromCSV(string csv)
+        {
+            cars = File.ReadAllLines(csv).Skip(1).Select(line => new Auto(line)).ToList();
+        }
+        public static List<Auto> GetCars()
+        {
+            return cars;
+        }
+    }
+}
--- AutokConsole/Program.cs
+++ AutokConsole/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutokConsole.Models;
+
+Auto.LoadFromCSV("Data\\autok.csv");
+List<Auto> autos = Auto.GetCars();
+
+Console.WriteLine($"5. feladat: {autos.Count} autó található a listában");
+Console.WriteLine($"6. feladat: Az autók esetében az átlagosan eladott darabszám {autos.Average(x => x.Sold)}");
+Console.WriteLine($"7. feladat: Az elmúlt 5 évben gyártott autók:");
+foreach (Auto auto in autos)
+{
+    if (auto.Year >= DateTime.Now.Year - 5)
+    {
+        Console.WriteLine($"\t{auto.Brand} {auto.Model}: {auto.Year}");
+    }
+}
+Console.WriteLine($"8. feladat: Legsikeresebb márkák listája az eladott darabszám alapján:");
+var brands = autos.GroupBy(x => x.Brand)
+    .Select(g => new { Brand = g.Key, TotalSold = g.Sum(x => x.Sold) })
+    .OrderByDescending(x => x.TotalSold)
+    .ToList();
+foreach (var brand in brands)
+{
+    Console.WriteLine($"\t{brand.Brand}: {brand.TotalSold} darab");
+}
